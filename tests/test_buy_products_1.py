a4b1fd74d15da1f325db603a48596d028d98e683
VitalyBesedin <vitaly_besedin@hotmail.com>
2023-06-07 22:35:16 +0300

    a few test with conftest

--- tests/test_buy_products_1.py
+++ tests/test_buy_products_1.py
@@ -49,57 +49,57 @@ def test_buy_product_1(set_up):
     driver.quit()
 
 # @pytest.mark.run(order=1)
-# def test_buy_product_2():
-#     # options = webdriver.ChromeOptions()
-#     # options.add_experimental_option("detach", True)
-#     # g = Service()
-#     # driver = webdriver.Chrome(options=options, service=g)  # this is and above macOS
-#     # driver = webdriver.Chrome()  # Windows
-#     # driver = webdriver.Firefox()
-#     driver = webdriver.Safari()
-#     # driver = webdriver.Edge()
-#
-#     print("Start Test 2")
-#
-#     login = LoginPage(driver)
-#     login.authorization()
-#
-#     mp = MainPage(driver)
-#     mp.select_products_2()
-#
-#     cp = CartPage(driver)
-#     cp.product_confirmation()
-#
-#
-#
-#     time.sleep(5)
-#     print("Finish Test 2")
-#     driver.quit()
+def test_buy_product_2(set_up):
+    # options = webdriver.ChromeOptions()
+    # options.add_experimental_option("detach", True)
+    # g = Service()
+    # driver = webdriver.Chrome(options=options, service=g)  # this is and above macOS
+    # driver = webdriver.Chrome()  # Windows
+    # driver = webdriver.Firefox()
+    driver = webdriver.Safari()
+    # driver = webdriver.Edge()
+
+    print("Start Test 2")
+
+    login = LoginPage(driver)
+    login.authorization()
+
+    mp = MainPage(driver)
+    mp.select_products_2()
+
+    cp = CartPage(driver)
+    cp.product_confirmation()
+
+
+
+    # time.sleep(5)
+    print("Finish Test 2")
+    driver.quit()
 
 # @pytest.mark.run(order=2)
-# def test_buy_product_3():
-#     # options = webdriver.ChromeOptions()
-#     # options.add_experimental_option("detach", True)
-#     # g = Service()
-#     # driver = webdriver.Chrome(options=options, service=g)  # this is and above macOS
-#     # driver = webdriver.Chrome()  # Windows
-#     # driver = webdriver.Firefox()
-#     driver = webdriver.Safari()
-#     # driver = webdriver.Edge()
-#
-#     print("Start Test 3")
-#
-#     login = LoginPage(driver)
-#     login.authorization()
-#
-#     mp = MainPage(driver)
-#     mp.select_products_3()
-#
-#     cp = CartPage(driver)
-#     cp.product_confirmation()
-#
-#
-#
-#     time.sleep(5)
-#     print("Finish Test 3")
-#     driver.quit()
+def test_buy_product_3():
+    # options = webdriver.ChromeOptions()
+    # options.add_experimental_option("detach", True)
+    # g = Service()
+    # driver = webdriver.Chrome(options=options, service=g)  # this is and above macOS
+    # driver = webdriver.Chrome()  # Windows
+    # driver = webdriver.Firefox()
+    driver = webdriver.Safari()
+    # driver = webdriver.Edge()
+
+    print("Start Test 3")
+
+    login = LoginPage(driver)
+    login.authorization()
+
+    mp = MainPage(driver)
+    mp.select_products_3()
+
+    cp = CartPage(driver)
+    cp.product_confirmation()
+
+
+
+    # time.sleep(5)
+    print("Finish Test 3")
+    driver.quit()
